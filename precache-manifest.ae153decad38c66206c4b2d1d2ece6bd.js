730e4d0939fad52d16c52c19179b3b89a114843b
sava642 <101993338+sava642@users.noreply.github.com>
2022-11-22 18:31:58 +0100

    Updates

---
+++ precache-manifest.ae153decad38c66206c4b2d1d2ece6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4ff0c7df19d676b2b1",
+    "url": "//redux-contactsbook/static/css/main.d93c8c63.chunk.css"
+  },
+  {
+    "revision": "da4ff0c7df19d676b2b1",
+    "url": "//redux-contactsbook/static/js/main.da4ff0c7.chunk.js"
+  },
+  {
+    "revision": "343732adf155781639da",
+    "url": "//redux-contactsbook/static/js/1.343732ad.chunk.js"
+  },
+  {
+    "revision": "f594be509fe646a7747a",
+    "url": "//redux-contactsbook/static/js/runtime~main.f594be50.js"
+  },
+  {
+    "revision": "4aaf8c3fb4e0adf3f88fbcd6bffd569f",
+    "url": "//redux-contactsbook/index.html"
+  }
+];
